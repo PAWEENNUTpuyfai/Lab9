33c8d45cd2f52f3d3abf7020016849f9081f8c3b
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2022-12-23 16:18:21 +0700

    lab9_3 .cpp completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,38 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+
+int main(){
+	int age,L,bounty;
+	string ch;
+	cout << "Enter your age: ";
+	cin >>age;
+	if(age<=25){
+		cout << "Enter your height: ";
+		cin >> L;
+		if(L<100){
+			ch = "Chopper";
+		}else if(L<180){
+			ch = "Usopp";
+		}else{
+			cout << "Enter your bounty: ";
+			cin >> bounty;
+			if(bounty>1100000000){
+				ch="Zoro";
+			}else{
+				ch="Sanji";
+			}
+		}
+	}else if(age<=60){
+		cout << "Enter your bounty: ";
+		cin >> bounty;
+		
+		if(bounty > 500000000){
+			ch = "Jinbe";
+		}else{
+			ch = "Franky";
+		}
+	}else{
+		ch ="Brook";
+		}
+	cout << "Your character = " << ch;
+}
